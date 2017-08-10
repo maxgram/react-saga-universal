b179ee5dd4e82a871ac9800ea3d6bb2704457046
Max Gram <dev@maxgram.com>
2017-08-10 19:46:02 -0400

    cleanup

--- src/redux/store/index.js
+++
@@ -1,6 +0,0 @@
-// //TODO: fix this
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports = require('./configureStoreProd')
-// } else {
-//   module.exports = require('./configureStoreDev')
-// }
