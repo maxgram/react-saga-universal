b77edf9ccd6ed16f9d305eae6a58772c445eaef6
Max Gram <dev@maxgram.com>
2017-08-09 21:33:45 -0400

    css var

--- config/aliases/index.js
+++ config/aliases/index.js
@@ -4,6 +4,7 @@ var CWD = process.cwd()
 module.exports = {
   resolve: {
     alias: {
+      'CSSVar': path.resolve(CWD, './src/root/AppRoot/styles/var'),
       'Components': path.resolve(CWD, './src/components/'),
       'Containers': path.resolve(CWD, './src/containers/'),
       'Pages': path.resolve(CWD, './src/containers/pages/'),
